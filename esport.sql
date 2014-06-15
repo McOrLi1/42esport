031ed6d53d2a5fa2bba8f05b05d2f9c4f130a09a
Lois PEARRON <lpearron@e2r12p21.42.fr>
2014-06-15 20:52:13 +0200

    base sql

--- esport.sql
+++ esport.sql
@@ -2,10 +2,10 @@
 -- version 4.1.9
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Generation Time: Jun 13, 2014 at 05:03 AM
--- Server version: 5.5.36
--- PHP Version: 5.4.26
+-- Client :  localhost:3306
+-- Généré le :  Dim 15 Juin 2014 à 11:51
+-- Version du serveur :  5.5.36
+-- Version de PHP :  5.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,95 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `esport`
+-- Base de données :  `esport`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `id_forum` int(11) NOT NULL,
+  `created` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `id_forum`, `created`) VALUES
+(1, 'General', 3, '10-25-1940'),
+(2, 'Equipes', 3, '14-15-2012'),
+(3, 'General', 2, '23-11-2004');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `forums`
+--
+
+CREATE TABLE IF NOT EXISTS `forums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `created` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `forums`
+--
+
+INSERT INTO `forums` (`id`, `name`, `created`) VALUES
+(1, 'LOL', '11-25-1999'),
+(2, 'WOW', '15-06-1669'),
+(3, 'CS', '11-25-1999');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `posts`
+--
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_thread` int(11) NOT NULL,
+  `id_author` int(11) NOT NULL,
+  `created` varchar(10) NOT NULL,
+  `modified` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `threads`
+--
+
+CREATE TABLE IF NOT EXISTS `threads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `id_category` int(11) NOT NULL,
+  `created` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `threads`
+--
+
+INSERT INTO `threads` (`id`, `name`, `id_category`, `created`) VALUES
+(1, 'Equipe Bubulle au taquet', 1, '11-12-1998'),
+(2, 'Equipe Louloute bug', 1, '32-98-1569');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
@@ -38,15 +120,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `status` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
 
 --
--- Dumping data for table `users`
+-- Contenu de la table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `name`, `lastname`, `email`, `type`, `birthdate`, `status`) VALUES
-(17, 'fadsfadsfasd', 'sdafsadf1sadfdsa', 'fasdf', 'asdfasf', 'asdfa@mail.com', 'Admin', '23-09-1989', 'Normal'),
-(18, 'admin_random', '1adminadmin', 'admin', 'admin', 'admin@mail.com', 'Admin', '12/12/1990', 'Normal');
+(17, 'lol', 'sdafsadf1sadfdsa', 'LOLILOL', 'asdfasf', 'asdfa@mail.com', 'User', '23-09-1989', 'Banni'),
+(18, 'admin_random', '1adminadmin', 'admin', 'admin', 'admin@mail.com', 'Admin', '12/12/1990', 'Normal'),
+(19, 'toto', 'zjkchzkjh1', 'toto', 'toto', 'toto@mail.com', 'Admin', '12-23-1999', 'Normal');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
